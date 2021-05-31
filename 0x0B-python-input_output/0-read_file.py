fca72ff1b261d4dbf148ada3ee2ce86e12870b4c
DovaZk <silva.joel.08@gmail.com>
2021-05-31 21:59:09 +0000

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,7 @@
 
 
 def read_file(filename=""):
-        """"read file"""
-            with open(filename, encoding="utf-8") as file:
-                        print(file.read(), end="")
+    """"read file"""
+    with open(filename, encoding="utf-8") as file:
+         print(file.read(), end="")
+x
