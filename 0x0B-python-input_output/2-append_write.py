429b648b799526112c1e34630db98daa313d12a2
DovaZk <silva.joel.08@gmail.com>
2021-06-01 02:02:43 +0000

    append write

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
+"""appends a string at the end of a text file"""
 
-""" Module to append text into a file """
 
 def append_write(filename="", text=""):
-    """ Function that appends the 'text' into the 'filename' """
-
-    with open(filename, 'a') as my_file:
-        return my_file.write(text)
+    """function"""
+    with open(filename, "a", encoding="utf-8") as file:
+        file.write(text)
+        return len(text)
